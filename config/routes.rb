902a060b2879591ca52f88503a2a4b354e70fc53
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-15 20:58:45 -0500

    added homepage view, added user views, set up root in routes, set up resources in routes for users, recipes, reviews, ingredients, and recipe_ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'application#index'
+
+  resources :users 
+  resources :recipes 
+  resources :reviews 
+  resources :ingredients 
+  resources :recipe_ingredients 
+
+
 end
