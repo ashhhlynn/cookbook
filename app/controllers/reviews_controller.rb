eeac59f09c6e2e7702137c8c6650777f6351646e
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-15 21:40:52 -0500

    same thing for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,42 @@
 class ReviewsController < ApplicationController
 
+
+def index
+end 
+    
+def new
+@review = Review.new
+end 
+        
+def create
+@review = Review.new(review_params)
+@review.save
+redirect_to recipe_path(@recipe)
+end 
+    
+def edit
+@review = Review.find(params[:id])
+end 
+        
+def update
+@review = Review.find(params[:id])
+@review.update(review_params)
+redirect_to user_path(current_user)
+end 
+    
+def destroy 
+@review = Review.find(params[:id])
+@review.destroy
+redirect_to user_path(current_user)
+end 
+    
+    
+        
+        
+private
+def review_params
+params.require(:review).permit(:text, :user_id, :recipe_id)
+end
+      
+
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
 
 def index 
-    @users = User.all 
+@users = User.all 
 end 
 
 def show 
-    @user = User.find(params[:id])
+@user = User.find(params[:id])
 end 
 
 def new 
-    @user = User.new 
+@user = User.new 
 end 
 
 def create
-    @user = User.new(user_params)
-    @user.save 
-    redirect_to user_path(@user)
+@user = User.new(user_params)
+@user.save 
+redirect_to user_path(@user)
 end 
 
 
 private 
 def user_params 
-    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+params.require(:user).permit(:email, :username, :password, :password_confirmation)
 end 
 
 
