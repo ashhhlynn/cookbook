7a38196b125d6f74d9a7d89d9324ae0b727273c0
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-16 23:48:00 -0500

    added a reject if attributes ingredient or quantity attribute is blank to accepts recipe ingredients nested attributes to controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ApplicationRecord
     has_many :recipe_ingredients, dependent: :destroy
     has_many :ingredients, through: :recipe_ingredients
 
-    accepts_nested_attributes_for :recipe_ingredients
+    accepts_nested_attributes_for :recipe_ingredients, :reject_if => proc { |attrs| attrs[:quantity].blank? || attrs[:ingredient_attributes][:name].blank?} 
 
 end 
