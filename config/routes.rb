12f7e95388f1791e501a4ced1b11130e4c442129
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-21 19:48:48 -0500

    routes in config

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
- #' get  '/signup',  to: 'users#new' '
-#' post '/signup',  to: 'users#create' '
+ get  '/signup',  to: 'users#new' 
+ post '/signup',  to: 'users#create' 
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
